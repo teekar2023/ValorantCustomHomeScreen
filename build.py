514805cb9afe71e8522ce01f08c4920c9caffced
teekar2023 <sree23palla@gmail.com>
2021-11-15 15:10:26 -0500

    v2.0.0

--- build.py
+++ build.py
@@ -2,5 +2,7 @@ import os
 from shutil import copy
 
 cwd = os.getcwd()
-os.system('pyinstaller -i download.ico ValorantCustomHomeScreen.py')
+os.system('pyinstaller -i download.ico ValorantCustomHomeScreen.pyw')
+copy(f"{cwd}\\CHANGELOG.txt", f"{cwd}\\dist\\ValorantCustomHomeScreen\\CHANGELOG.txt")
+copy(f"{cwd}\\ABOUT.txt", f"{cwd}\\dist\\ValorantCustomHomeScreen\\ABOUT.txt")
 copy(f"{cwd}\\LICENSE.txt", f"{cwd}\\dist\\ValorantCustomHomeScreen\\LICENSE.txt")
